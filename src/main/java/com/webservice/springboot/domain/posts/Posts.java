6dd6287f2eb6c2f997a413c9d48774104bb3219e
JinYoungChoi95 <jinyoungchoi95@gmail.com>
2020-12-11 18:07:49 +0900

    Post class 생성 - DB table과 매칭

---
+++ src/main/java/com/webservice/springboot/domain/posts/Posts.java
@@ -0,0 +1,46 @@
+package com.webservice.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//lombok annotation
+//get Getter Method
+@Getter
+//기본 생성자 자동 추가
+//==public Posts(){}
+@NoArgsConstructor
+
+//JPA annotation
+//table과 실제 링크될 class임을 표현
+@Entity
+public class Posts {
+    //Primary Key field
+    @Id
+    //Primary Key 생성 규칙
+    //GenerationType.IDENTITY를 지정해주어야 id가 auto increment됨
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+
+    //feild column 옵션 설정시 사용
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
